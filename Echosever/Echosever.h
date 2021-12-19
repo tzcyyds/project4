ae39d5e72e05aebea3fe96da637586b2ef9bc777
yudipan <3010201649@qq.com>
2021-12-19 16:15:32 +0800

    添加项目文件。

---
+++ Echosever/Echosever.h
@@ -0,0 +1,32 @@
+﻿
+// Echosever.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CEchoseverApp:
+// 有关此类的实现，请参阅 Echosever.cpp
+//
+
+class CEchoseverApp : public CWinApp
+{
+public:
+	CEchoseverApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CEchoseverApp theApp;
